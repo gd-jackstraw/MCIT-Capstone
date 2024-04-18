e046fe51bfc274ee84b396659e8a6e9bb4a72ab1
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-04-18 13:22:01 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -89,10 +89,9 @@ resource "google_storage_bucket" "bucket" {
 }
 
 resource "google_storage_bucket_iam_member" "members" {
-  for_each = {
-    for m in var.iam_members : "${m.role} ${m.member}" => m
-  }
+ 
   bucket = google_storage_bucket.bucket.name
-  role   = each.value.role
-  member = each.value.member
+  iam_members = [{
+    role   = "roles/storage.objectViewer"
+    member = "terraform-auth@mcit-capstone-dev.iam.gserviceaccount.com"
 }
