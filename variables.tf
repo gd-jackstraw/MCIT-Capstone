4da59754099759309b07a8fe00e8a1b523771587
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-04-18 13:08:11 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  description = "The name of the bucket."
+  type        = string
+}
+
+variable "project_id" {
+  description = "The ID of the project to create the bucket in."
+  type        = string
+}
+
+variable "location" {
+  description = "The location of the bucket."
+  type        = string
+}
