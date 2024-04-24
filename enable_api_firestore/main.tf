29592ab8127a846b74538a9e528a8c9906788f62
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-04-23 20:06:11 -0400

    Update main.tf

--- enable_api_firestore/main.tf
+++ enable_api_firestore/main.tf
@@ -1,12 +1,34 @@
-provider "google" {
-  credentials = file("terraform-auth-prod@mcit-capstone-prod.iam.gserviceaccount.com")
-  project     = "mcit-capstone-prod"
-  region      = "us-central1"
-}
 
 resource "google_project_service" "firestore" {
-  project = "<YOUR_GCP_PROJECT_ID>"
+  project = "mcit-capstone-prod"
   service = "firestore.googleapis.com"
 
   disable_on_destroy = false
+}
+
+
+resource "google_storage_bucket" "static" {
+  name         = "gcp_capstone"
+  location      = "us-central1"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+  encryption {
+    default_kms_key_name = google_kms_crypto_key.terraform_state_bucket.id
+  }
+  depends_on = [
+    google_project_iam_member.default
+  ]
+}
+
+resource "google_project_iam_member" "default" {
+  project = "mcit-capstone-prod"
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:terraform@${google_project.project.number}.iam.gserviceaccount.com"
+}
+
+resource "google_kms_crypto_key" "terraform_state_bucket" {
+  name     = "terraform-state-bucket-key"
+  location = "us-central1"
 }
