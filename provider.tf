b06ac88975257e932d630c08d3ff47944f8a9e6c
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-04-28 16:54:09 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,6 +8,7 @@ terraform {
 }
 provider "google" {
   project = "mcit-capstone-dev"
+  credentials = file("<PATH_TO_YOUR_SERVICE_ACCOUNT_JSON>")
   region = "us-east1"
 }
 
@@ -15,6 +16,12 @@ resource "google_project_service" "firestore" {
     service = "firestore.googleapis.com"
 }
 
+resource "google_project_service" "compute_api" {
+  project = google_project.project.project_id
+  service = "compute.googleapis.com"
+
+  disable_on_destroy = false
+}
 
 provider "google-beta" {
 }
