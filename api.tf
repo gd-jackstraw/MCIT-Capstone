1ff6a673196d1c49e8303925ecccfa0babcf79d4
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-04-28 16:40:01 -0400

    Update api.tf

--- api.tf
+++ api.tf
@@ -1,9 +1,5 @@
 /* FIRESTORE API */
 
-provider "google" {
-    project = "mcit-capstone-dev"
-    region  = "us-east1"
-}
 
 resource "google_project_service" "firestore" {
     service = "firestore.googleapis.com"
