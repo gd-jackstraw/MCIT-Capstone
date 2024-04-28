341946ab3d9b2c273afdf5cb25871a5adaa88070
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-04-28 18:43:47 -0400

    Update workflows.tf

--- workflows.tf
+++ workflows.tf
@@ -17,19 +17,19 @@ module "cloud_workflow" {
   workflow_source       = <<-EOF
 - initialize:
     assign:
-      - project: ${sys.get_env("GOOGLE_CLOUD_PROJECT_ID")}
+      - project: $${sys.get_env("GOOGLE_CLOUD_PROJECT_ID")}
       - firestoreDatabaseId: (default)
       - firestoreBackupBucket: gs://dev-test-bucket12864
 - exportFirestoreDatabaseAll:
     call: http.post
     args:
-      url: ${"https://firestore.googleapis.com/v1/projects/"+project+"/databases/"+firestoreDatabaseId+":exportDocuments"}
+      url: $${"https://firestore.googleapis.com/v1/projects/"+project+"/databases/"+firestoreDatabaseId+":exportDocuments"}
       auth:
         type: OAuth2
       body:
         outputUriPrefix: ${firestoreBackupBucket}
     result: result
 - returnResult:
-    return: ${result}
+    return: $${result}
 EOF
 }
