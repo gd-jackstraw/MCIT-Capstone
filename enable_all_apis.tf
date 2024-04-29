934c01ef77699741b7f36185849185cd259f7e8d
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-04-29 19:52:29 -0400

    Update enable_all_apis.tf

--- enable_all_apis.tf
+++ enable_all_apis.tf
@@ -1,35 +1,25 @@
 # Enable Firestore API
 resource "google_project_service" "firestore" {
-  project = "mcit-capstone-prod" 
+  project = var.project_id
   service = "firestore.googleapis.com"
 }
 
 # Enable Compute Engine API
 resource "google_project_service" "compute_engine" {
-  project = "mcit-capstone-prod"
+  project = var.project_id
   service = "compute.googleapis.com"
 }  
 
 # Enable Workflows API  
 resource "google_project_service" "workflows" {
-  project = "mcit-capstone-prod"
+  project = var.project_id
   service = "workflows.googleapis.com"
 }
 
 # Enable Cloud Scheduler API
 resource "google_project_service" "cloud_scheduler" {
-  project = "mcit-capstone-prod"
+  project = var.project_id
   service = "cloudscheduler.googleapis.com"
 }
 
-# Create App Engine app
-resource "google_app_engine_application" "app" {
-  location_id = "us-central"
 
-  depends_on = [
-    google_project_service.firestore,
-    google_project_service.compute_engine, 
-    google_project_service.workflows,
-    google_project_service.cloud_scheduler
-  ]
-}
