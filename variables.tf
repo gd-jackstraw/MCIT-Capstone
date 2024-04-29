551112eb7516fc7f4cb9b76e75d037971370c445
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-04-29 19:57:26 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,72 @@
-variable "GOOGLE_CREDENTIALS" {
+variable "GOOGLE_CREDENTIALS" {}
+
+variable "name" {
+  description = "The name of the bucket."
   type        = string
-  description = "Path to the Google Cloud credentials file"
+}
+
+variable "project_id" {
+  description = "The ID of the project to create the bucket in."
+  type        = string
+}
+
+variable "location" {
+  description = "The location of the bucket."
+  type        = string
+}
+
+variable "storage_class" {
+  description = "The Storage Class of the new bucket."
+  type        = string
+  default     = null
+}
+
+variable "labels" {
+  description = "A set of key/value label pairs to assign to the bucket."
+  type        = map(string)
+  default     = null
+}
+
+variable "bucket_policy_only" {
+  description = "Enables Bucket Policy Only access to a bucket."
+  type        = bool
+  default     = true
+}
+
+variable "versioning" {
+  description = "While set to true, versioning is fully enabled for this bucket."
+  type        = bool
+  default     = false
+}
+
+variable "autoclass" {
+  description = "While set to true, autoclass is enabled for this bucket."
+  type        = bool
+  default     = false
+}
+
+variable "force_destroy" {
+  description = "When deleting a bucket, this boolean option will delete all contained objects. If false, Terraform will fail to delete buckets which contain objects."
+  type        = bool
+  default     = true
+}
+
+variable "retention_policy" {
+  description = "Configuration of the bucket's data retention policy for how long objects in the bucket should be retained."
+  type = object({
+    is_locked        = bool
+    retention_period = number
+  })
+  default = null
+}
+
+variable "retention_period_seconds" {
+  description = "object retention period in seconds"
+  type = number
+}
+
+variable "public_access_prevention" {
+  description = "Prevents public access to a bucket. Acceptable values are inherited or enforced. If inherited, the bucket uses public access prevention, only if the bucket is subject to the public access prevention organization policy constraint."
+  type        = string
+  default     = "inherited"
 }
