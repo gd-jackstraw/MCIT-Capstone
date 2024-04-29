ae84ca23e1e70ccf7a372b43e36a32ca40f93869
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-04-29 19:54:58 -0400

    Update create_firestore_db.tf

--- create_firestore_db.tf
+++ create_firestore_db.tf
@@ -1,6 +1,7 @@
-resource "google_firestore_database" "database" {
+/* resource "google_firestore_database" "database" {
   project     = "mcit-capstone-prod"
   name        = "prod-fire-db-data"
   location_id = "nam5"
   type        = "FIRESTORE_NATIVE"
 }
+*/
