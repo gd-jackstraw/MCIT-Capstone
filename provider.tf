ad2565c3c3319d5e50da2097b8c3889e20c7df55
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-04-29 19:51:11 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,20 @@
 terraform {
+  required_version = ">= 1.3"
   required_providers {
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 4.0"
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.53, < 5.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.3"
     }
   }
 }
-provider "google" {
-project = "mcit-capstone-prod"
+
+  provider "google" {
+    project = var.project_id
 }
 
 provider "google-beta" {
