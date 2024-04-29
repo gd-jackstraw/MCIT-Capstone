83675251151b8700998d2af8f1583eb06101b731
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-04-28 22:48:51 -0400

    Create create_firestore_db.tf

---
+++ create_firestore_db.tf
@@ -0,0 +1,6 @@
+resource "google_firestore_database" "database" {
+  project     = "mcit-capstone-prod"
+  name        = "prod-fire-db-data"
+  location_id = "nam5"
+  type        = "FIRESTORE_NATIVE"
+}
