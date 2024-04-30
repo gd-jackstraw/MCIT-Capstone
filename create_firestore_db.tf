930d4298c53785c7e2b0481b9d930c52dca0ec07
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-04-30 13:56:10 -0400

    Update create_firestore_db.tf

--- create_firestore_db.tf
+++ create_firestore_db.tf
@@ -1,3 +1,9 @@
+/**
+ * Configures a Google Firestore database resource.
+ *
+ * This resource creates a Firestore database instance in the specified project and location.
+ * The database is configured to use the Firestore Native mode.
+ */
 resource "google_firestore_database" "database" {
   project     = "mcit-capstone-prod"
   name        = "prod-fire-db-data"
