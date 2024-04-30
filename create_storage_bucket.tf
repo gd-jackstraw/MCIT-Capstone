9fac49155ad412be150e5067d55600bed37d34d4
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-04-30 13:57:37 -0400

    Update create_storage_bucket.tf

--- create_storage_bucket.tf
+++ create_storage_bucket.tf
@@ -1,3 +1,13 @@
+/**
+ * Defines a Google Cloud Storage bucket and an object within that bucket.
+ *
+ * The `google_storage_bucket` resource creates a new storage bucket with the
+ * specified name, location, and storage class. The `uniform_bucket_level_access`
+ * setting enables uniform bucket-level access control.
+ *
+ * The `google_storage_bucket_object` resource uploads an image file named
+ * "mcit.png" to the storage bucket, setting the content type to "text/plain".
+ */
 resource "google_storage_bucket" "static" {
  name          = "capstone-prod-test-bucket"
  project = "mcit-capstone-prod"
