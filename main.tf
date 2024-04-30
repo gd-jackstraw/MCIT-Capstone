5db81e8a7efcebc3ea00d4b1ea199cc97cfe1381
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-04-29 20:06:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,3 +21,45 @@ resource "google_storage_bucket" "bucket" {
   }
 }
 
+
+module "cloud_workflow" {
+  source  = "GoogleCloudPlatform/cloud-workflows/google"
+  version = "~> 0.1"
+
+  project_id = var.project_id
+
+  workflow_name         = "wf-firestore-backup"
+  region                = var.region
+  service_account_email = var.service_account_email
+  workflow_trigger = {
+    cloud_scheduler = {
+      name                  = "workflow-job"
+      cron                  = "0 0 * * *"
+      time_zone             = "America/New_York"
+      deadline              = "320s"
+      service_account_email = var.service_account_email
+    }
+  }
+  workflow_source       = <<-EOF
+
+- initialize:
+    assign:
+      - project: $${sys.get_env("GOOGLE_CLOUD_PROJECT_ID")}
+      - firestoreDatabaseId: (default)
+      - firestoreBackupBucket: gs://firestore-backup-04292024
+- exportFirestoreDatabaseAll:
+    call: http.post
+    args:
+      url: $${"https://firestore.googleapis.com/v1/projects/"+project+"/databases/"+firestoreDatabaseId+":exportDocuments"}
+      auth:
+        type: OAuth2
+      body:
+        outputUriPrefix: $${firestoreBackupBucket}
+    result: result
+- returnResult:
+    return: $${result}
+
+EOF
+
+
+}
